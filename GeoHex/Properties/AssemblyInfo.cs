b7a3360110bd6bcf972ad5f95087c6839df86ddc
mattak <mattak.me@gmail.com>
2016-07-07 23:10:16 +0900

    chore(version): bump version up 0.0.3.1 fix for packaging misstake

--- GeoHex/Properties/AssemblyInfo.cs
+++ GeoHex/Properties/AssemblyInfo.cs
@@ -37,6 +37,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.0.3.0")]
-[assembly: AssemblyFileVersion("0.0.3.0")]
+[assembly: AssemblyVersion("0.0.3.1")]
+[assembly: AssemblyFileVersion("0.0.3.1")]
 
